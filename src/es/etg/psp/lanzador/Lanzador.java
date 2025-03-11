a1c058850dc188f00597e5c17d5abda0ce73e959
kelvinbex2 <obamedo20@gmail.com>
2025-03-11 12:21:34 -0500

    xxxx

--- src/es/etg/psp/lanzador/Lanzador.java
+++ src/es/etg/psp/lanzador/Lanzador.java
@@ -1,8 +1,10 @@
 package es.etg.psp.lanzador;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Lanzador {
@@ -10,56 +12,80 @@ public class Lanzador {
     public final static String MSG_BUENO = "Ha ido bien";
     public final static String ERR_MALO = "No ha ido bien";
     public final static String TERMINAL = "> ";
-    public final static String PIPE = " | ";
+    public final static String PIPE = "|";
 
-    public static void main(String[] args) {
 
-        try {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws  RuntimeException {
+
+        try (Scanner scanner = new Scanner(System.in)){
             System.out.print(TERMINAL);
             String commando = scanner.nextLine().trim();
 
-            System.out.print(TERMINAL);
-            String commado2 = scanner.nextLine().trim();
-
-            String[] lan = { commando, PIPE, commado2 };
-            ejecutar(lan);
+            
+            ejecutar(commando);
         } catch (Exception e) {
-            e.printStackTrace();
+            throw  new RuntimeException(e.getMessage());
         }
 
     }
 
-    public static int ejecutar(String[] comand) {
+    public static int ejecutar(String comand)  throws RuntimeException, InterruptedException{
         int exitVal = 0;
         StringBuilder build = new StringBuilder();
-        String line = "";
+        String line;
         try {
 
-            String [] comandoCompleto = unir(comand);
-            Process process = Runtime.getRuntime().exec(comandoCompleto);
+           String [] comandoCompleto = unir(comand) ;
+
+           String primer = comandoCompleto[0];
+           String second = comandoCompleto[1];
+
+
+            Process process = Runtime.getRuntime().exec(primer);
             BufferedReader read = new BufferedReader(new InputStreamReader(process.getInputStream()));
             while ((line = read.readLine()) != null) {
-                build.append(line).append(System.lineSeparator());
+                build.append(line).append("\n");
+             
             }
 
+
+
+
+            exitVal = process.waitFor();
+
+
+            Process process2 = Runtime.getRuntime().exec(second);
+            BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(process2.getOutputStream()));
+            wr.write(build.toString());
+            wr.close();
+
+
+
+            BufferedReader read2 = new BufferedReader(new InputStreamReader(process2.getInputStream()));
+            while ((line = read2.readLine()) != null) {
+                System.out.println(line);
+             
+            }
+
+
+
+            exitVal = process.waitFor();
             if (exitVal == 0) {
                 System.out.println(MSG_BUENO);
-                System.out.println(build);
             } else {
                 System.out.println(ERR_MALO);
             }
 
         } catch (IOException e) {
 
-            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
         }
         return exitVal;
     }
 
-    public static String[] unir(String[] comand) {
-        String comandoCompleto = String.join("", comand);
-        return new String[] { comandoCompleto };
+    public static String[] unir(String comand) {
+        return comand.split("\\|");
+
     }
 
 }
