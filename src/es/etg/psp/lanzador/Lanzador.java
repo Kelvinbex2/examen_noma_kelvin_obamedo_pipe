34ff6bfc7bf463f7a08000f690e552920de69088
kelvinbex2 <obamedo20@gmail.com>
2025-03-11 15:56:40 +0100

    fin

--- src/es/etg/psp/lanzador/Lanzador.java
+++ src/es/etg/psp/lanzador/Lanzador.java
@@ -6,35 +6,38 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Lanzador {
-    
+
     public final static String MSG_BUENO = "Ha ido bien";
     public final static String ERR_MALO = "No ha ido bien";
     public final static String TERMINAL = "> ";
-
-    
+    public final static String PIPE = " | ";
 
     public static void main(String[] args) {
-      
 
         try {
             Scanner scanner = new Scanner(System.in);
             System.out.print(TERMINAL);
-            String commando = scanner.nextLine();
-            ejecutar(commando);
+            String commando = scanner.nextLine().trim();
+
+            System.out.print(TERMINAL);
+            String commado2 = scanner.nextLine().trim();
+
+            String[] lan = { commando, PIPE, commado2 };
+            ejecutar(lan);
         } catch (Exception e) {
             e.printStackTrace();
         }
-       
 
     }
 
-    public static int ejecutar(String comand) {
+    public static int ejecutar(String[] comand) {
         int exitVal = 0;
         StringBuilder build = new StringBuilder();
         String line = "";
         try {
 
-            Process process = Runtime.getRuntime().exec(comand);
+            String [] comandoCompleto = unir(comand);
+            Process process = Runtime.getRuntime().exec(comandoCompleto);
             BufferedReader read = new BufferedReader(new InputStreamReader(process.getInputStream()));
             while ((line = read.readLine()) != null) {
                 build.append(line).append(System.lineSeparator());
@@ -52,6 +55,11 @@ public class Lanzador {
             e.printStackTrace();
         }
         return exitVal;
+    }
+
+    public static String[] unir(String[] comand) {
+        String comandoCompleto = String.join("", comand);
+        return new String[] { comandoCompleto };
     }
 
 }
